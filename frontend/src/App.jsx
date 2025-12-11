7f62646ab60c8ec0d81b01b2bf21310f2613f53b
anilsinthu114 <anilsinthu114@gmail.com>
2025-12-11 10:13:04 +0530

    Production --V1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import { PortalLayout } from "./layouts/PortalLayout";
 
 // Pages
 import { Home } from "./pages/Home";
-import { PaymentReturn } from "./pages/paymentReturn";
+import { PaymentReturn } from "./pages/PaymentReturn";
 import { Success } from "./pages/Success";
 import { ErrorPage } from "./pages/ErrorPage";
 import { ExamFeeForm } from "./pages/forms/ExamFeeForm";
